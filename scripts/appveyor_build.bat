744fddb61e052ed31ad3d8de76f56a9718cefe8f
Gustav Hansen <kusti8@gmail.com>
2019-05-27 13:33:52 -0400

    Remove publish build latest [publish binary]

--- scripts/appveyor_build.bat
+++ scripts/appveyor_build.bat
@@ -8,7 +8,7 @@ ECHO packaging for node-gyp
 CALL node_modules\.bin\node-pre-gyp package --build-latest-napi-version-only
 ::make commit message env var shorter
 SET CM=%APPVEYOR_REPO_COMMIT_MESSAGE%
-IF NOT "%CM%" == "%CM:[publish binary]=%" (ECHO publishing && CALL node_modules\.bin\node-pre-gyp-github publish --build-latest-napi-version-only ) ELSE (ECHO not publishing)
+IF NOT "%CM%" == "%CM:[publish binary]=%" (ECHO publishing && CALL node_modules\.bin\node-pre-gyp-github publish ) ELSE (ECHO not publishing)
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 GOTO DONE
