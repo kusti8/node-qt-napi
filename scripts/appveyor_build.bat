6afad04a2db18fdd5f853022221c4ae3116d0373
Gustav Hansen <kusti8@gmail.com>
2019-05-27 13:26:57 -0400

    Try out napi 1-3 building [publish binary]

--- scripts/appveyor_build.bat
+++ scripts/appveyor_build.bat
@@ -5,10 +5,10 @@ SET EL=0
 ECHO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %~f0 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ECHO packaging for node-gyp
-CALL node_modules\.bin\node-pre-gyp package
+CALL node_modules\.bin\node-pre-gyp package --build-latest-napi-version-only
 ::make commit message env var shorter
 SET CM=%APPVEYOR_REPO_COMMIT_MESSAGE%
-IF NOT "%CM%" == "%CM:[publish binary]=%" (ECHO publishing && CALL node_modules\.bin\node-pre-gyp-github publish ) ELSE (ECHO not publishing)
+IF NOT "%CM%" == "%CM:[publish binary]=%" (ECHO publishing && CALL node_modules\.bin\node-pre-gyp-github publish --build-latest-napi-version-only ) ELSE (ECHO not publishing)
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 GOTO DONE
